b450977eaa8551f23a600c744d0d578b8343316b
nekrosb <makarov.nikita.mn@gmail.com>
2025-12-05 16:22:44 +0100

    feat: style of base

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import './App.css';
+import { ButtonComplete, ButtonOpenCreateTodo, ButtonCreateTodo, ButtonDeleteAll, ButtonDeleteTodo, ButtonCloseTodo } from './button';
+import { InputTitle, InputDate } from './input';
 
-const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+function Todo() {
+    return <div className='todo-item'>
+        <ul>
+            <li><h3>title</h3></li>
+            <li>date</li>
+            <li><ButtonComplete /></li>
+            <li><ButtonDeleteTodo /></li>
+        </ul>
     </div>
-  );
-};
+}
 
-export default App;
+export default function App() {
+    return (
+        <div className='main-container'>
+            <header>
+                <h1>todo app</h1>
+                <ButtonOpenCreateTodo />
+                <ButtonDeleteAll />
+            </header>
+            <div className='todos-container'>
+                <Todo />
+                <Todo />
+                <Todo />
+            </div>
+
+<div className='create-menu-content'>
+    <h2>Create New Todo</h2>
+    <InputTitle />
+    <InputDate />
+    
+    <ButtonCreateTodo />
+    <ButtonCloseTodo />
+</div>
+
+
+        </div>
+    )
+
+}
---
+++ src/button.tsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+export function ButtonOpenCreateTodo() {
+    return <button type="button" className="btn btn-open">+</button>
+}
+
+
+export function ButtonDeleteAll() {
+    return <button type="button" className="btn btn-delete-all">Delete All</button>
+}    
+
+export function ButtonComplete() {
+    return <button type="button" className="btn btn-complete">Complete</button>
+}
+
+export function ButtonDeleteTodo() {
+    return <button type="button" className="btn btn-delete">Delete</button>
+}
+
+
+export function ButtonCreateTodo() {
+    return <button type="button" className="create-menu-buttons btn-create">Create Todo</button>
+}
+
+
+export function ButtonCloseTodo() {
+    return <button type="button" className="create-menu-buttons btn-close">Close</button>
+}
